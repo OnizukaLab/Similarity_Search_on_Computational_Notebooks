a61a3e97b2c3961dd8f1cf20c02db6d59a3973f4
Horiuchi <horiuchi.misato@ist.osaka-u.ac.jp>
2021-05-16 19:16:58 +0900

    Initial commit

---
+++ retrieval_system/interface/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+"""
+class Query(WorkflowMatching):
+    self.QueryGraph=nx.DiGraph()
+    self.attr_of_q_node_type = nx.get_node_attributes(self.QueryGraph, "node_type")
+    self.attr_of_q_real_cell_id=nx.get_node_attributes(self.QueryGraph, "real_cell_id")
+    self.attr_of_q_display_type=nx.get_node_attributes(self.QueryGraph, "display_type")
+    self.query_lib=self.fetch_all_library_from_db("lephonepricingpredictions")
+    self.query_workflow_info={"Cell": 3, "Var": 1, "Display_data": {"text":1, "png":1}, "max_indegree": 1, "max_outdegree": 2}
+    self.set_query_workflow_info_Display_data()
+"""
